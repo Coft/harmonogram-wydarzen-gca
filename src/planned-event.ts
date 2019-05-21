b266db4a2a89c92de94ade702f87282b059d3110
coft <coft@o2.pl>
2019-05-21 06:13:12 +0200

    update eventTimelineType selection

--- src/planned-event.ts
+++ src/planned-event.ts
@@ -10,29 +10,42 @@ export enum EventTimelineType {
 
 export class PlannedEvent {
     readonly timelineType: EventTimelineType;
-    readonly startDate: Date;
+    readonly startDate: moment.Moment;
     readonly startDateFormated: string;
+    readonly startDateHardFormated: string;
+    readonly endDate: moment.Moment;
     readonly endDateFormated: string;
+    readonly endDateHardFormated: string;
     readonly topic: string;
     readonly place: string;
     readonly imagePath: string;
 
 	constructor(startDate: Date, endDate: Date, topic: string, place: string, imagePath: string = "") {
-        this.startDate = startDate;
+        this.startDate = moment(startDate);
         this.startDateFormated = moment(startDate).locale(settings["moment-locale"]).calendar();
+        this.startDateHardFormated = moment(startDate).locale(settings["moment-locale"]).format("LLLL");
+        this.endDate = moment(endDate);
         this.endDateFormated = moment(endDate).locale(settings["moment-locale"]).calendar();
+        this.endDateHardFormated = moment(endDate).locale(settings["moment-locale"]).format("LLLL");
         this.topic = topic;
         this.place = place;
         this.imagePath = imagePath;
 
         this.timelineType = EventTimelineType.Present;
         
-        let today: Date = new Date();
-        if (today > startDate) {
-            this.timelineType = EventTimelineType.Past;
-        }
-        else if (startDate > today) {
+        let dayDifference: number = moment().diff(startDate, 'days');
+
+        if (dayDifference > 14) {
             this.timelineType = EventTimelineType.Future;
+        }
+        else if (dayDifference > 0) {
+            this.timelineType = EventTimelineType.Near;
+        }
+        else if (dayDifference == 0) {
+            this.timelineType = EventTimelineType.Present;
+        }
+        else {
+            this.timelineType = EventTimelineType.Past;
         }
     }
 }
